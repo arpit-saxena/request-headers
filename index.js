d8845bbe4dce2910f57b8962319d3ba197f56cf8
arpit-saxena <arpit.saxena2000@yahoo.in>
2016-04-02 22:07:22 +0530

    Improved the software info

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ app.use("/",express.static(__dirname + "/public"));
 
 app.get("/whoami", function(req, res) {
   req.setEncoding('utf8');
-  var ip = req.connection.remoteAddress;
-  console.log(req.connection);
+  var ip = req.headers['x-forwarded-for'] || req.ip;
   var user_agent = req.headers['user-agent'];
+  var os = user_agent.split(/\(|\)/)[1];
   var lang = req.headers['accept-language'].split(/,|;/)[0];
   var info = {
     "ipaddress": ip,
     "language": lang,
-    "software": user_agent
+    "software": os
   };
   res.json(info);
 });
