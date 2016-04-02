894a2434fe5376a76ead3805ef763fd956e592de
arpit-saxena <arpit.saxena2000@yahoo.in>
2016-04-02 18:17:19 +0530

    Added functionality

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app.use("/",express.static(__dirname + "/public"));
 
 app.get("/whoami", function(req, res) {
   req.setEncoding('utf8');
+  var ip = req.ip;
+  var user_agent = req.headers['user-agent'];
+  var lang = req.headers['accept-language'].split(/,|;/)[0];
+  var info = {
+    "ipaddress": ip,
+    "language": lang,
+    "software": user_agent
+  };
+  res.json(info);
 });
 
 app.listen(process.env.PORT || 3000);
